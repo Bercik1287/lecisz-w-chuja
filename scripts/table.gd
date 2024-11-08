3924155d054844669a4e0defde1c2b409225cffa
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-11-08 21:32:48 +0100

    version 0.2

--- scripts/table.gd
+++ scripts/table.gd
@@ -11,6 +11,7 @@ var p1tries = 0
 var p2tries = 0
 var p3tries = 0
 var p4tries = 0
+var color_change = 2
 
 var rng = RandomNumberGenerator.new()
 
@@ -19,32 +20,82 @@ func _ready() -> void:
 	p2 = rng.randi_range(1, 6)
 	p3 = rng.randi_range(1, 6)
 	p4 = rng.randi_range(1, 6)
+	bp1_color_override(0,1,0,1)
+	bp2_color_override(0,1,0,1)
+	bp3_color_override(0,1,0,1)
+	bp4_color_override(0,1,0,1)
+
+func bp1_color_override(a: float, b: float, c: float, d: float) -> void:
+	$bp1.add_theme_color_override("font_color", Color(a,b,c,d))
+	$bp1.add_theme_color_override("font_hover_color", Color(a,b,c,d))
+	$bp1.add_theme_color_override("font_hover_pressed_color", Color(a,b,c,d))
+	$bp1.add_theme_color_override("font_pressed_color", Color(a,b,c,d))
+	$bp1.add_theme_color_override("font_focus_color", Color(a,b,c,d))
+	$bp1.add_theme_color_override("font_color", Color(a,b,c,d))
+
+func bp2_color_override(a: float, b: float, c: float, d: float) -> void:
+	$bp2.add_theme_color_override("font_color", Color(a,b,c,d))
+	$bp2.add_theme_color_override("font_hover_color", Color(a,b,c,d))
+	$bp2.add_theme_color_override("font_hover_pressed_color", Color(a,b,c,d))
+	$bp2.add_theme_color_override("font_pressed_color", Color(a,b,c,d))
+	$bp2.add_theme_color_override("font_focus_color", Color(a,b,c,d))
+	$bp2.add_theme_color_override("font_color", Color(a,b,c,d))
+
+func bp3_color_override(a: float, b: float, c: float, d: float) -> void:
+	$bp3.add_theme_color_override("font_color", Color(a,b,c,d))
+	$bp3.add_theme_color_override("font_hover_color", Color(a,b,c,d))
+	$bp3.add_theme_color_override("font_hover_pressed_color", Color(a,b,c,d))
+	$bp3.add_theme_color_override("font_pressed_color", Color(a,b,c,d))
+	$bp3.add_theme_color_override("font_focus_color", Color(a,b,c,d))
+	$bp3.add_theme_color_override("font_color", Color(a,b,c,d))
+
+func bp4_color_override(a: float, b: float, c: float, d: float) -> void:
+	$bp4.add_theme_color_override("font_color", Color(a,b,c,d))
+	$bp4.add_theme_color_override("font_hover_color", Color(a,b,c,d))
+	$bp4.add_theme_color_override("font_hover_pressed_color", Color(a,b,c,d))
+	$bp4.add_theme_color_override("font_pressed_color", Color(a,b,c,d))
+	$bp4.add_theme_color_override("font_focus_color", Color(a,b,c,d))
+	$bp4.add_theme_color_override("font_color", Color(a,b,c,d))
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
-
 func _on_bp_1_pressed() -> void:
-	p1tries = p1tries + 1
-	$bp1.text = "Gracz 1 (" + str(p1tries) + "/6)"
-	if(p1tries == p1):
-		$bp1.text = "Gracz 1 (Martwy)"
-
+	if(p1tries!=p1):
+		p1tries = p1tries + 1
+		if(p1tries>color_change):
+			bp1_color_override(1,1,0,1)
+		$bp1.text = "Gracz 1 (" + str(p1tries) + "/6)"
+		if(p1tries == p1):
+			$bp1.text = "Gracz 1 (Martwy)"
+			bp1_color_override(1,0,0,1)
 
 func _on_bp_2_pressed() -> void:
-	p2tries = p2tries + 1
-	$bp2.text = "Gracz 2 (" + str(p2tries) + "/6)"
-	if(p2tries == p2):
-		$bp2.text = "Gracz 2 (Martwy)"
+	if(p2tries!=p2):
+		p2tries = p2tries + 1
+		if(p2tries>color_change):
+			bp2_color_override(1,1,0,1)
+		$bp2.text = "Gracz 2 (" + str(p2tries) + "/6)"
+		if(p2tries == p2):
+			$bp2.text = "Gracz 2 (Martwy)"
+			bp2_color_override(1,0,0,1)
 
 func _on_bp_3_pressed() -> void:
-	p3tries = p3tries + 1
-	$bp3.text = "Gracz 3 (" + str(p3tries) + "/6)"
-	if(p3tries == p3):
-		$bp3.text = "Gracz 3 (Martwy)"
+	if(p3tries!=p3):
+		p3tries = p3tries + 1
+		if(p3tries>color_change):
+			bp3_color_override(1,1,0,1)
+		$bp3.text = "Gracz 3 (" + str(p3tries) + "/6)"
+		if(p3tries == p3):
+			$bp3.text = "Gracz 3 (Martwy)"
+			bp3_color_override(1,0,0,1)
 
 func _on_bp_4_pressed() -> void:
-	p4tries = p4tries + 1
-	$bp4.text = "Gracz 4 (" + str(p4tries) + "/6)"
-	if(p4tries == p4):
-		$bp4.text = "Gracz 4 (Martwy)"
+	if(p4tries!=p4):
+		p4tries = p4tries + 1
+		if(p4tries>color_change):
+			bp4_color_override(1,1,0,1)
+		$bp4.text = "Gracz 4 (" + str(p4tries) + "/6)"
+		if(p4tries == p4):
+			$bp4.text = "Gracz 4 (Martwy)"
+			bp4_color_override(1,0,0,1)
